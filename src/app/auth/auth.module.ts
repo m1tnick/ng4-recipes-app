f43c4d7291ab8db9c9001c99ccfd1411d6f048bc
m1tnick <m1tnick@hotmail.com>
2017-12-22 23:46:47 +0000

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { SigninComponent } from "./signin/signin.component";
+import { SignupComponent } from "./signup/signup.component";
+import { FormsModule } from "@angular/forms";
+import { AuthRouting } from "./auth-routing.module";
+
+
+@NgModule({
+    declarations: [
+        SigninComponent,
+        SignupComponent
+    ],
+    imports: [
+        FormsModule,
+        AuthRouting
+    ],
+})
+export class AuthModule {}
---
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipes-detail',
+  templateUrl: './recipes-detail.component.html',
+  styleUrls: ['./recipes-detail.component.css']
+})
+export class RecipesDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
+
+  constructor(private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
+
+}
