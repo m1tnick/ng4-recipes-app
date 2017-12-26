5f9ab4619c00df704f464b053ad7538f32ffc57a
Vânio Ferreira <vanioferreira@MBP-de-Vanio.lan>
2017-12-26 14:49:47 +0000

    317: Adjusting the App Module StoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { StoreModule } from '@ngrx/store';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
-
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [
@@ -24,7 +23,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     SharedModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   providers: [],
   bootstrap: [AppComponent]
